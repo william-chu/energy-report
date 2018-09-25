f4823a2b46d1217cfb5262f3ca81d22ad7ce342f
William Chu <Williamchu51@gmail.com>
2018-09-25 08:22:23 -0700

    add logic to grab process data for single month

--- src/App.js
+++ src/App.js
@@ -6,47 +6,52 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      janACUsedArr: [],
-      janHeatUsedArr: [],
+      ACUsedArr: [],
+      HeatUsedArr: [],
     };
   }
 
     componentDidMount() {
+      // Unix timestamps, each day is 86400
       const jan2018Start = 1514764800;
-      const janDayLength = 31;
+      const mar2018Start = 1519862400;
+      const may2018Start = 1525132800;
+      const monthDayLength = 31;
       const position = {
         latitude: 45.5898,
         longitude: -122.5951
       };
       const acTemp = 75;
       const heatTemp = 62;
-      const proxy = 'https://cors-anywhere.herokuapp.com/';
-      const darkSkyURL = proxy + 'https://api.darksky.net/forecast/' + process.env.REACT_APP_DARK_SKY_API_KEY + '/' + position.latitude + ',' + position.longitude + ',' + jan2018Start + "?exclude=currently,minutely,daily,alerts,flag";
-      fetch(darkSkyURL)
-      .then(results => {
-        return results.json();
-      })
-      .then(data => {
-        let newJanACUsedArr = this.state.janACUsedArr.slice();
-        let newJanHeatUsedArr = this.state.janHeatUsedArr.slice();
-        let acActivated = false;
-        let heatActivated = false;
-        data.hourly.data.map((hour) => {
-          if (parseInt(hour.temperature) > acTemp) {
-            return acActivated = true;
-          } else if (parseInt(hour.temperature) < heatTemp) {
-            return heatActivated = true;
-          }
-            return;
-        });
-        newJanACUsedArr.push(acActivated);
-        newJanHeatUsedArr.push(heatActivated);
-        console.log('acusage', newJanACUsedArr);
-        console.log('heatusage', newJanHeatUsedArr);
-        this.setState({janACUsedArr: newJanACUsedArr});
-        this.setState({janHeatUsedArr: newJanHeatUsedArr});
-        console.log('state', this.state);
-      })
+      let darkSkyURL = '';
+      for (let i = 0; i < monthDayLength; i++ ){
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        let dateTimeStamp = jan2018Start + (i * 86400);
+        darkSkyURL = proxy + 'https://api.darksky.net/forecast/' + process.env.REACT_APP_DARK_SKY_API_KEY + '/' + position.latitude + ',' + position.longitude + ',' + dateTimeStamp + "?exclude=currently,minutely,daily,alerts,flag";
+        fetch(darkSkyURL)
+        .then(results => {
+          return results.json();
+        })
+        .then(data => {
+          let newACUsedArr = this.state.ACUsedArr.slice();
+          let newHeatUsedArr = this.state.HeatUsedArr.slice();
+          let acActivated = false;
+          let heatActivated = false;
+          data.hourly.data.map((hour) => {
+            if (hour.temperature > acTemp) {
+              return acActivated = true;
+            }
+            if (hour.temperature < heatTemp) {
+              return heatActivated = true;
+            }
+            return null;
+          });
+          newACUsedArr.push(acActivated);
+          newHeatUsedArr.push(heatActivated);
+          this.setState({ACUsedArr: newACUsedArr});
+          this.setState({HeatUsedArr: newHeatUsedArr});
+        })
+      }
     }
 
   render() {
