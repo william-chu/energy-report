b7ffa1986d799537c5d757137e9cf2eefc98bf3f
William Chu <Williamchu51@gmail.com>
2018-09-25 09:00:19 -0700

    add Navigation component and basic styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './Navigation';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       ACUsedArr: [],
-      HeatUsedArr: [],
+      heatUsedArr: [],
+      monthReportStart: 1514764800,
     };
+    const jan2018Start = 1514764800;
+    const mar2018Start = 1519862400;
+    const may2018Start = 1525132800;
   }
 
     componentDidMount() {
       // Unix timestamps, each day is 86400
-      const jan2018Start = 1514764800;
-      const mar2018Start = 1519862400;
-      const may2018Start = 1525132800;
       const monthDayLength = 31;
       const position = {
         latitude: 45.5898,
@@ -24,34 +26,34 @@ class App extends Component {
       const acTemp = 75;
       const heatTemp = 62;
       let darkSkyURL = '';
-      for (let i = 0; i < monthDayLength; i++ ){
-        const proxy = 'https://cors-anywhere.herokuapp.com/';
-        let dateTimeStamp = jan2018Start + (i * 86400);
-        darkSkyURL = proxy + 'https://api.darksky.net/forecast/' + process.env.REACT_APP_DARK_SKY_API_KEY + '/' + position.latitude + ',' + position.longitude + ',' + dateTimeStamp + "?exclude=currently,minutely,daily,alerts,flag";
-        fetch(darkSkyURL)
-        .then(results => {
-          return results.json();
-        })
-        .then(data => {
-          let newACUsedArr = this.state.ACUsedArr.slice();
-          let newHeatUsedArr = this.state.HeatUsedArr.slice();
-          let acActivated = false;
-          let heatActivated = false;
-          data.hourly.data.map((hour) => {
-            if (hour.temperature > acTemp) {
-              return acActivated = true;
-            }
-            if (hour.temperature < heatTemp) {
-              return heatActivated = true;
-            }
-            return null;
-          });
-          newACUsedArr.push(acActivated);
-          newHeatUsedArr.push(heatActivated);
-          this.setState({ACUsedArr: newACUsedArr});
-          this.setState({HeatUsedArr: newHeatUsedArr});
-        })
-      }
+      // for (let i = 0; i < monthDayLength; i++ ){
+      //   const proxy = 'https://cors-anywhere.herokuapp.com/';
+      //   let dateTimeStamp = this.state.monthReportStart + (i * 86400);
+      //   darkSkyURL = proxy + 'https://api.darksky.net/forecast/' + process.env.REACT_APP_DARK_SKY_API_KEY + '/' + position.latitude + ',' + position.longitude + ',' + dateTimeStamp + "?exclude=currently,minutely,daily,alerts,flag";
+      //   fetch(darkSkyURL)
+      //   .then(results => {
+      //     return results.json();
+      //   })
+      //   .then(data => {
+      //     let newACUsedArr = this.state.ACUsedArr.slice();
+      //     let newHeatUsedArr = this.state.heatUsedArr.slice();
+      //     let acActivated = false;
+      //     let heatActivated = false;
+      //     data.hourly.data.map((hour) => {
+      //       if (hour.temperature > acTemp) {
+      //         return acActivated = true;
+      //       }
+      //       if (hour.temperature < heatTemp) {
+      //         return heatActivated = true;
+      //       }
+      //       return null;
+      //     });
+      //     newACUsedArr.push(acActivated);
+      //     newHeatUsedArr.push(heatActivated);
+      //     this.setState({ACUsedArr: newACUsedArr});
+      //     this.setState({heatUsedArr: newHeatUsedArr});
+      //   })
+      // }
     }
 
   render() {
@@ -61,9 +63,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">React Energy Report</h1>
         </header>
-        <p className="App-intro">
-          A report to help The Port of Portland determine if PDX Airport HVAC needs replacement
-        </p>
+        <Navigation />
       </div>
     );
   }
---
+++ src/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navigation.css';
+
+function Navigation() {
+  return(
+    <div className="navigation-container">
+      <h1>Click to view 2018 report by month</h1>
+      <button>Jan</button>
+      <button>Mar</button>
+      <button>May</button>
+    </div>
+  )
+}
+
+export default Navigation;
